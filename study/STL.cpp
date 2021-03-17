4bcc856beaa2528898f0f1d7e2b058bb9fdff735
unknown <824228502@qq.com>
2021-03-17 17:43:44 +0800

    modify STL.cpp

--- study/STL.cpp
+++ study/STL.cpp
@@ -1,4 +1,3 @@
-//iterator_traits
 #include"new.cpp"
 #include"type_traits.cpp"
 //list--链表 双向链表
@@ -11,6 +10,7 @@ struct __list_node{
     T data;
 };
 
+namespace zjh{
 //list迭代器--iterator
 template<class T,class Ref,class Ptr>
 struct  __list_iterator
@@ -62,7 +62,7 @@ public:
 };
 
 //list的数据结构
-template<class T,class Alloc/* =alloc */>
+template<class T,class Alloc>
 class list{
 protected:
     typedef __list_node<T> list_node;
@@ -280,5 +280,314 @@ public:
         }
     }
 
-    //list的sort函数只能使用自己的，因为算法中的sort只支持随机迭代器
+    //list的sort函数只能使用自己的，因为算法中的sort只支持随机迭代器,而list是一个双向迭代器
+    //主要运行了merge函数，通过merge来进行合并，参考归并的方法进行合并操作，1-2-4-8-..依次类推
+    void sort()
+    {
+        //如果只有一个元素或者没有元素，就不需要进行操作了,虽然size()也可以判断，但是速度太慢了
+        if(node->next == node || link_type(node->next)->next == node)
+            return;
+        
+        //一些新的lists,作为中介数据存放区
+        list<T,Alloc> carry;
+        list<T,Alloc> counter[64];
+
+        int fill = 0;
+        while (!empty())
+        {
+            carry.splice(carry.begin(),*this,begin());
+            int i = 0;
+            while(i < fill && !counter[i].empty())
+            {
+                counter[i].merge(carry);
+                carry.swap(counter[i++]);
+                if(i == fill) fill++;
+            }
+
+            for(int i = 1; i < fill;i++)
+                counter[i].merge(counter[i-1]);
+            swap(counter[fill-1]);
+        }
+    }
+
+    /* 使用全局的指针交换即可 */
+    void swap(list<T,Alloc>& list)
+    {
+        using namespace swap;
+        ::swap(*this,list);
+    }
+};
+
+//deque的迭代器
+template<class T,class Ref,class Ptr,size_t BufSiz>
+struct __deque_iterator
+{
+    typedef __deque_iterator<T,T&,T*> iterator;
+    typedef __deque_iterator<T,const T&,const T*,BufSiz> const_iterator;
+    static size_t buffer_size(){ return __deque_buf_size(BufSiz,sizeof(T));}
+    typedef random_iterator_tag iterator_category;
+    typedef T value_type;
+    typedef Ptr pointer;
+    typedef Ref reference;
+    typedef size_t size_type;
+    typedef ptrdiff_t difference_type;
+    typedef T** map_pointer;
+
+    typedef __deque_iterator self;
+
+    //保持与容器的联结
+    T* cur; //此迭代器所指缓存区中的现行元素
+    T* first; //此迭代器所指之缓冲区的头
+    T* last; //此迭代器所指缓冲区的尾，包含对应的未使用空间
+    map_pointer node;
+
+    inline size_t __deque_buf_size(size_t n,size_t sz)
+    {
+        return n != 0 ? n:(sz < 512 ? size_t(512,sz):size_t(1););
+    }
+
+    void set_node(map_pointer new_node)
+    {
+        node = new_node;
+        first = *new_node;
+        last = first + difference_type(buffer_size());
+    }
+
+    //返回当前的指向
+    reference operator*()const{return *cur;}
+    //返回当前指向的指针地址，即指针对象
+    pointer operator->()const{return &(operator*());}
+
+    difference_type operator-(const self& x){
+        return difference_type(buffer_size())*(node-x.node - 1) + (cur - first) + (x.last - x.cur);
+    }
+
+    self& operator++(){
+        ++cur;
+        if(cur == last)
+        {
+            set_node(node+1);
+            cur = first;
+        }
+        return *this;
+    }
+
+    self& operator++(int){
+        self tmp = *this;
+        ++*this;
+        return tmp;
+    }
+
+    self& operator--(){
+        if(cur == first)
+        {
+            set_node(node- 1);
+            cur = last;
+        }
+        --cur;
+        return *this;
+    }
+
+    self& operator--(int){
+        self tmp = *this;
+        --*this;
+        return tmp;
+    }
+
+    //实现随机存取,迭代器可以直接条约n个距离
+    self& operator+=(difference_type n){
+        difference_type offset = n + (cur - first);
+        //如果存在在同一个内存空间中
+        if(offset > 0 && offset < difference_type(buffer_size()))
+            cur += n;
+        else
+        {
+            //节点的偏差值
+            difference_type node_offset = offset > 0 ? offset/difference_type(buffer_size()): -difference_type((-offset - 1)/buffer_size())-1;
+            //切换到正确的节点
+            set_node(node_offset);
+            //切换到正确的元素
+            cur = first + (offset - node_offset*difference_type(buffer_size()));
+        }
+        return *this;
+    }
+
+    self operator+(difference_type n)const{
+        self tmp = *this;
+        return tmp+=n;
+    }
+
+    self& operator-=(difference_type n)const{
+        return *this += -n;
+    }
+
+    self operator-(difference_type n) const{
+        self tmp = *this;
+        return tmp-=n;
+    }
+
+    //实现随机迭代器取数据
+    reference operator[](difference_type n)const{
+        return *(this + n);
+    }
+
+    bool operator==(const self& x)const{return cur == x.cur;}
+    bool operator!=(const self& x)const{return !(*this == x);}
+    bool operator<(const self& x)const
+    {
+        return (node == x.node)?(cur < x.cur):(node < x.node;);
+    }
+};
+
+//deque双向队列，也是一个连续的线性空间不断拼接在一起的，所以迭代器很复杂
+//最后一个参数指向缓冲区大小，默认为512Byte
+template<class T,class Alloc,size_t BufSiz = 0>
+class deque{
+public:
+    typedef T value_type;
+    typedef value_type* pointer;
+    typedef value_type& reference;
+    typedef size_t size_type;
+    typedef ptrdiff_t difference_type;
+    typedef __deque_iterator<T,T&,T*,BufSiz> iterator;
+
+protected:
+    typedef pointer* map_pointer; //指向T指针的指针
+    iterator start;
+    iterator finish;
+    map_pointer map;
+    size_type map_size; //map中可容纳指针的数量
+
+//专属空间分配器
+    typedef simple_alloc<value_type,Alloc> data_allocator; //类型分配器
+    typedef simple_alloc<pointer,Alloc> map_allocator; //指针分配器
+
+public:
+
+    deque(int n,const value_type& value):start(),finish(),map(0),map_size(0)
+    {
+        fill_initialize(n,value);
+    };
+
+    void fill_initialize(size_type n,const value_type& value)
+    {
+        create_map_and_nodes();
+        map_pointer cur;
+        for(cur = start.node;cur!=finish.node;cur++)
+            uninitialized_fill(*cur,*cur + buffer_size(),value);
+        uninitialized_fill(finish.first,finish.last,value);
+    }
+
+    /* 创建map和对应节点 */
+    void create_map_and_nodes(size_type num_elements)
+    {
+        //需要的节点数(元素个数/每个缓存器可容纳的节点个数)+1;
+        //如果刚好整除，会多配一个节点
+        size_type num_nodes = num_elements / buffer_size() + 1;
+
+        //一个map要管理几个节点，最少8个，最多所需节点数+2
+        /* 前后都预留一个，支持扩充 */
+        map_size = max(initial_map_size(),num_nodes + 2);
+        map = map_allocator::allocate(map_size);
+
+        //以下令nstart和nfinish指向map所拥有之全部节点的最中央区域
+        //保持在最中央，可使头尾两端的扩充能量一样大，每个节点可对应一个缓冲区
+        map_pointer nstart = map + (map_size - num_nodes) / 2;
+        map_pointer nfinish = nstart + num_nodes - 1;
+        
+        map_pointer cur;
+        //为map内的每个现用节点都配置缓冲区，所有缓冲区加起来就是deque可用的总缓冲区
+        for(cur = nstart;cur!=nfinish;cur++)
+            *cur = allocate_node();
+
+        //为deque的两个迭代器对象设置节点
+        start.set_node(nstart);
+        finish.set_node(nfinish);
+        start.cur = start.first;
+        finish.cur = finish.first + num_elements % buffer_size();
+        //之前解释过，当节点刚好被整除，那么此时的finish的cur指向多出来的那个节点的头节点
+    }
+
+    void push_back(const value_type& t){
+        //当前缓冲区还没有满
+        if(finish.cur != finish.last - 1)
+        {
+            construct(finish.cur,t);
+            finish++;
+        }
+        else
+            push_back_aux(t); //当前缓冲区已经满了
+    }
+
+    void push_back_anx(const value_type& t)
+    {
+        value_type t_copy = t;
+        reserve_map_at_back(); //若符合某种条件就需要替换一个map
+        *(finish.node+1) = allocate_node(); //配置一个新的缓冲区
+        construct(finish.cur,t_copy); //针对标的元素设值
+        finish.set_node(finish.node+1); //改变finish，令其指向新节点
+        finish.cur = finish.first; //设定finish的状态
+    }
+    
+    void push_front(const value_type& t){
+        if (start.cur != start.first)
+        {
+            construct(start.cur,t);
+            start.cur--;
+        }
+        else
+            push_front_anx(t);
+    }
+
+    void push_back_anx(const value_type& t)
+    {
+        value_type t_copy = t;
+        reserve_map_at_front();//若符合某种条件，需要对map进行重新替换
+        *(start.node - 1) = allocate_node();
+        __STL_TRY{
+            start.set_node(start.node - 1);
+            start.cur = start.last - 1;
+            construct(start.cur,t_copy);
+        }
+        cathc(){
+            // commit or rollback
+            start.set_node(start.node + 1);
+            start.cur = start.first;
+            deallocate_node(*(start.node - 1));
+            throw;
+        }
+    }
+
+    void reserve_map_at_back(size_type nodes_to_add = 1)
+    {
+        if(nodes_to_add + 1 > map_size - (finish.node - map))
+            reallocate_map(nodes_to_add,false);
+    }
+
+    void reserve_map_at_front(size_type nodes_to_add = 1)
+    {
+
+    }
+
+
+    iterator begin(){return start;}
+    iterator end(){return finish;}
+
+    reference operator[](size_type n){
+        //调用迭代器的随机存取函数
+        return start[difference_type(n)]; 
+    }
+
+    reference front(){return *start;} //调用迭代器的*
+    reference back(){
+        iterator tmp = finish;
+        --tmp; //调用迭代器operator--
+        return *tmp; //调用迭代器operator*
+    }
+
+    size_type size()const {return finish - start;;}
+    size_type max_size()const{return size_type(-1);}
+    bool empty()const{return start == finish;}
+};
+
 };
